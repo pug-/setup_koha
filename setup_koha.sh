15df2499806e9d7ffcfa0c6dacde0069638db90b
Purab Gujar <purab@cameoml.com>
2013-07-31 14:28:27 +0530

    added the package installation instructions required to create koha instance

--- setup_koha.sh
+++ setup_koha.sh
@@ -1,2 +1,16 @@
 #!/bin/bash 
 # Setup script for configuring a koha production env on Ubuntu 12.04 LTS
+
+echo deb http://debian.koha-community.org/koha squeeze main | sudo tee /etc/apt/sources.list.d/koha.list
+wget -O- http://debian.koha-community.org/koha/gpg.asc | sudo apt-key add -
+sudo apt-get update
+
+# install the main koha debian package from the source added above
+sudo apt-get install -y koha-common
+
+# install mysql server
+sudo apt-get install -y mysql-server
+
+# enable apache2 module
+sudo a2enmod rewrite
+sudo service apache2 restart
